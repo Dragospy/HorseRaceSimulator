1be2e5d36e368cf6966c1db08e01400a0d0730b1
dragospy <dragosesoalca@gmail.com>
2025-04-02 17:05:46 +0100

    Attempting to fix Package issues

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -1,14 +1,17 @@
-package Part1;
-
 /**
- * Write a description of class Horse here.
+ * This class is the blueprint for an object that represents a Horse.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Dragos Soalca 
+ * @version 0.1
  */
 public class Horse
 {
     //Fields of class Horse
+    private String name;
+    private char symbol;
+    private double confidence;
+    private boolean  fallen = false;
+    private int distanceTravelled = 0;
     
     
       
@@ -18,60 +21,65 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+       this.name = horseName;  //Horse's name
+       this.symbol = horseSymbol;  //Horse's symbol
+       this.confidence = horseConfidence;  //Horse's confidence
     }
-    
     
     
     //Other methods of class Horse
     public void fall()
     {
-        
+        fallen = true;  //Horse has fallen                      
     }
     
     public double getConfidence()
     {
-        
+        return confidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return distanceTravelled;
     }
     
     public String getName()
     {
-        
+        return name;
     }
     
     public char getSymbol()
     {
-        
+        return symbol;              
     }
     
     public void goBackToStart()
     {
-        
+        distanceTravelled = 0;  //Horse goes back to start
+        fallen = false;  //Horse is not fallen anymore
     }
     
     public boolean hasFallen()
     {
-        
+        return fallen;  //Horse has fallen
     }
 
     public void moveForward()
     {
-        
+        //Move the horse fowrard by one unit 
+        if (!fallen){
+            distanceTravelled++;
+        }
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        confidence = newConfidence;
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        symbol = newSymbol;
     }
     
 }
---
+++ Part1/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Race currentRace = new Race(15);
+
+        currentRace.startRace();
+    }
+}
