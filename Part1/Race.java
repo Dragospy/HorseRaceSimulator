f7e628bf47f789d04631001682d41a54fd458164
dragospy <dragosesoalca@gmail.com>
2025-04-08 01:02:39 +0100

    Made the raceLength field final as it doesn't change once its been set in the constructor

--- Part1/Race.java
+++ Part1/Race.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Race
 {
-    private int raceLength;
+    private final int raceLength;
     private Horse lane1Horse;
     private Horse lane2Horse;
     private Horse lane3Horse;
