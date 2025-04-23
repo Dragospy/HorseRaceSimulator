0001d1756c0bc37dd4dd55a056cf6d755556280b
dragospy <dragosesoalca@gmail.com>
2025-04-24 01:10:06 +0300

    Added basic pages for each of the different GUI requirements

--- Part2/guiMain.java
+++ Part2/guiMain.java
@@ -1,19 +1,98 @@
 package Part2;
 
 import Part1.*;
+import java.util.ArrayList;
 import javax.swing.*;
+
 public class guiMain {
 
     private final static valueContainer laneCount = new valueContainer(3);
     private final static valueContainer trackLength = new valueContainer(15);
+    private static JPanel raceTrack = null;
+    private final static ArrayList<Horse> horses = new ArrayList<>();
 
     public static void main(String[] args) {
         JTabbedPane tabs = new JTabbedPane();
         tabs.addTab("Track Setup", trackPanel());
+        tabs.addTab("Horse Customization", horseCustomization());
+        tabs.addTab("Race Stats", statistics());
+        tabs.addTab("Race Bets", betting());
         tabs.addTab("Race Start", racePanel());
 
         initialiseFrame("Race Horse Simulator", tabs);
     }
+
+    public static void initialiseFrame(String title, JComponent tabs) {
+        JFrame frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(tabs);
+        frame.setSize(800, 500);
+        frame.setVisible(true);
+    }
+
+
+    //Panels
+
+    public static JPanel betting(){
+        //Main panel setup
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        return mainPanel;
+    }
+
+    public static JPanel statistics(){
+        //Main panel setup
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        return mainPanel;
+    }
+
+    public static JPanel horseCustomization(){
+        //Main panel setup
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        return mainPanel;
+    }
+
+    public static JPanel trackPanel(){
+        //Main panel setup
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        JLabel title = new JLabel("Track Customization");
+        title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        mainPanel.add(title);
+
+        JPanel contentPanel = new JPanel();
+
+        //Add panels
+        contentPanel.add(idElement("Number of Lanes","Lanes", "lane", laneCount, 0, 15));
+        contentPanel.add(idElement("Track Length" ,"Track length", "length", trackLength, 5, 40));
+
+        mainPanel.add(contentPanel);
+
+        return mainPanel;
+    }
+
+    public static JPanel racePanel(){
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Race Start"));
+
+        //Start Race
+        JButton startButton = Button("Start", 25, 25);
+        
+        startButton.addActionListener(e -> {
+            startTheRace();
+        });
+
+        panel.add(startButton);
+        
+        return panel;
+    }
+
+    //Methods
 
     public static void startTheRace(){
         Race currentRace = new Race(trackLength.value, true);
@@ -25,36 +104,27 @@ public class guiMain {
         currentRace.startRace();
     }
 
-    public static JButton createButton(String text, int x, int y) {
+    //Elements
+
+    public static JButton Button(String text, int x, int y) {
         JButton button = new JButton(text);
         button.setSize(button.getPreferredSize());
         button.setLocation(x, y);
         return button;
     }
 
-    public static JPanel racePanel(){
-        JPanel panel = new JPanel();
-        panel.add(new JLabel("Race Start"));
-
-        JButton startButton = createButton("Start", 25, 25);
-
-        startButton.addActionListener(e -> {
-            startTheRace();
-        });
-
-        panel.add(startButton);
-
-        return panel;
-    }
-
-//Increment / Decrement element
-    private static JPanel idElement(String valueName, String displayName, valueContainer value, int min, int max){
+    //Increment / Decrement element
+    private static JPanel idElement(String title, String valueName, String displayName, valueContainer value, int min, int max){
         //Custom lane count
-        JPanel laneCountPanel = new JPanel();
+        JPanel valuePanel = new JPanel();
+        JLabel elementTitle = new JLabel(title);
+        elementTitle.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         JLabel laneCountLabel = new JLabel(displayName+": " + value.value);
+        laneCountLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         // Increments lane count by 1
-        JButton incrementButton = createButton("Increment " + valueName, 0, 0);
+        JButton incrementButton = Button("Increment " + valueName, 0, 0);
+        incrementButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
         incrementButton.addActionListener(e -> {
             if (value.value < max){
                 value.value++;
@@ -63,7 +133,8 @@ public class guiMain {
         });
 
         // Decrements lane count by 1
-        JButton decrementButton = createButton("Decrement " + valueName, 0, 0);
+        JButton decrementButton = Button("Decrement " + valueName, 0, 0);
+        decrementButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
         decrementButton.addActionListener(e -> {
             if (value.value > min){
                 value.value--;
@@ -71,37 +142,17 @@ public class guiMain {
             }
         });
 
+        valuePanel.setLayout(new BoxLayout(valuePanel, BoxLayout.Y_AXIS));
+
         //Add components to panel
-        laneCountPanel.add(incrementButton);
-        laneCountPanel.add(laneCountLabel);
-        laneCountPanel.add(decrementButton);
+        valuePanel.add(elementTitle);
+        valuePanel.add(incrementButton);
+        valuePanel.add(laneCountLabel);
+        valuePanel.add(decrementButton);
 
-        return laneCountPanel;
-    }
-
-    public static JPanel trackPanel(){
-        //Main panel setup
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        JLabel title = new JLabel("Track Customization");
-        title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-        mainPanel.add(title);
-
-        //Add panels
-        mainPanel.add(idElement("Lanes", "lane", laneCount, 0, 15));
-        mainPanel.add(idElement("Track length", "length", trackLength, 5, 40));
-        return mainPanel;
-    }
-
-    public static void initialiseFrame(String title, JComponent tabs) {
-        JFrame frame = new JFrame(title);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(tabs);
-        frame.setSize(800, 500);
-        frame.setVisible(true);
+        return valuePanel;
     }
 }
-
 
 class valueContainer{
     int value;
